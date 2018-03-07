0d38ae70fe0b42c817369d829e4fc04602297242
trangle1302 <lengochaitrang@gmail.com>
2018-03-07 15:23:54 +0100

    add function to find border object, function for watershed by topographic distance

--- Segmentation_pipeline_helper.py
+++ Segmentation_pipeline_helper.py
@@ -9,6 +9,7 @@ import os
 import skimage
 from scipy import ndimage as ndi
 from PIL import Image
+import numpy as np
 
 # function to find recursively all files with specific prefix and suffix in a directory
 def find(dirpath, prefix=None, suffix=None, recursive=True):
@@ -45,38 +46,54 @@ def watershed_lab(image, marker = None):
     segmentation = ndi.binary_fill_holes(segmentation - 1)
     labeled_obj, num = ndi.label(segmentation)
     
-    # remove bordered objects
-    bw = skimage.morphology.closing(labeled_obj > 0, skimage.morphology.square(3))
-    cleared = skimage.segmentation.clear_border(bw)
-    labeled_obj, num = ndi.label(cleared)
+    # remove bordered objects, now moved to later steps of segmentation pipeline
+#    bw = skimage.morphology.closing(labeled_obj > 0, skimage.morphology.square(3))
+#    cleared = skimage.segmentation.clear_border(bw)
+#    labeled_obj, num = ndi.label(cleared)
     
     # remove too small or too large object 
     output = np.zeros(labeled_obj.shape)
-    for region in regionprops(labeled_obj):
-        if region.area >= 20000:# <= thres_high:
+    for region in skimage.measure.regionprops(labeled_obj):
+        if region.area >= 2000:# <= thres_high:
             # if the component has a volume within the two thresholds,
             # set the output image to 1 for every pixel of the component
             output[labeled_obj == region.label] = 1
                 
     labeled_obj, num = ndi.label(output)
                 
-    return labeled_obj
+    return labeled_obj, num
+
+def watershed_lab2(image, marker = None):
+    
+    distance = ndi.distance_transform_edt(image)
+    distance = skimage.segmentation.clear_border(distance, buffer_size=50)
+    # determine markers for watershed if not specified
+    if marker is None:
+        local_maxi = skimage.feature.peak_local_max(distance, indices=False, footprint=np.ones((3, 3)),
+                            labels=image)
+        marker = ndi.label(local_maxi)[0]
+
+    #segmentation with watershed algorithms
+    segmentation = skimage.morphology.watershed(-distance, marker, mask = image)
+                
+    return segmentation
 
 
 # Function to resize and pad segmented image, keeping the aspect ratio 
 #(maybe keep the relative ratio of the cells to each other as well?)
-def resize_pad(image): #input an Image object (PIL)
+def resize_pad(image, size = 256): #input an Image object (PIL)
     
-    desired_size = 256
+    image = Image.fromarray(image)
+    desired_size = size 
     
     # current size of the image
     old_size=image.size
     # old_size[0] is in (width, height) format
-    ratio = 0.5#float(desired_size)/max(old_size) 
+    ratio = 0.2 #float(desired_size)/max(old_size) 
     
     # size of the image after reduced by half
     new_size = tuple([int(x*ratio) for x in old_size])
-
+    
     # resize image
     im = image.resize(new_size, Image.ANTIALIAS)
     
@@ -87,3 +104,51 @@ def resize_pad(image): #input an Image object (PIL)
     
     return new_im
 
+
+
+# Function to find the indices of objects touching border
+def find_border(labels, buffer_size=0, bgval=0, in_place=False):
+    """Find objects connected to the label image border.
+    Adjusted from skimage.segmentation.clear_border()
+    Parameters
+    ----------
+    labels : (M[, N[, ..., P]]) array of int or bool
+        Imaging data labels.
+    buffer_size : int, optional
+        The width of the border examined.  By default, only objects
+        that touch the outside of the image are removed.
+    bgval : float or int, optional
+        Cleared objects are set to this value.
+    in_place : bool, optional
+        Whether or not to manipulate the labels array in-place.
+    Returns
+    -------
+    out : (M[, N[, ..., P]]) array
+        Imaging data labels with cleared borders
+    """
+    image = labels
+
+    if any( ( buffer_size >= s for s in image.shape)):
+        raise ValueError("buffer size may not be greater than image size")
+
+    # create borders with buffer_size
+    borders = np.zeros_like(image, dtype=np.bool_)
+    ext = buffer_size + 1
+    slstart = slice(ext)
+    slend   = slice(-ext, None)
+    slices  = [slice(s) for s in image.shape]
+    for d in range(image.ndim):
+        slicedim = list(slices)
+        slicedim[d] = slstart
+        borders[slicedim] = True
+        slicedim[d] = slend
+        borders[slicedim] = True
+
+    # Re-label, in case we are dealing with a binary image
+    # and to get consistent labeling
+    #labels = skimage.measure.label(image, background=0)
+
+    # determine all objects that are connected to borders
+    borders_indices = np.unique(labels[borders])
+    
+    return borders_indices
