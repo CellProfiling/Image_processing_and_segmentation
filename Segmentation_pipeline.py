7327c6bcc0057c12a2e23c82ea511c16287c25b1
trangle1302 <lengochaitrang@gmail.com>
2018-03-12 16:58:37 +0100

    Add tracking messages

--- Segmentation_pipeline.py
+++ Segmentation_pipeline.py
@@ -13,15 +13,15 @@ from scipy import ndimage as ndi
 from PIL import Image
 import gzip
 
-import Segmentation_pipeline_helper
+from Segmentation_pipeline_helper import find, watershed_lab, watershed_lab2, resize_pad, find_border, pixel_norm, shift_center_mass
 
 ##### EXECUTION PIPELINE FOR CELL SEGMENTATION
 
 # Define path to image input directory
-imageinput = "~/U2OS_noccd/TIF_GZ"
+imageinput = "U2OS_noccd/TIF_GZ"
 
 # Define desired path to save the segmented images
-imageoutput = "~/Desktop/U2OS_noccd/PNG"
+imageoutput = "U2OS_noccd/PNG"
 
 if not os.path.exists(imageoutput):
     os.makedirs(imageoutput)
@@ -41,11 +41,16 @@ for f in nuclei:
 # Cut the bounding box of each cell (3channels) in the respective image, slack and save
 for index,imgpath in enumerate(nuclei):
     
+    print("Segmenting image {0}/{1}".format(index,len(nucleoli)))
     # Unzip .gz file and read content image to img
-    with gzip.open(imgpath) as f:
-        nu = plt.imread(f)
-        if len(nu.shape) > 2:
-            nu=nu[:,:,2]
+    try:
+        with gzip.open(imgpath) as f:
+            nu = plt.imread(f)
+            if len(nu.shape) > 2:
+                nu=nu[:,:,2]
+    except:
+        print("%s does not have valid nucleus channel" % (nuclei[index].split("TIF_GZ/"))[1].split("blue")[0])
+        continue
     
     try:
         with gzip.open(nucleoli[index]) as f:
@@ -53,6 +58,7 @@ for index,imgpath in enumerate(nuclei):
             if len(org.shape) > 2:
                 org=org[:,:,1]
     except:
+        print("%s does not have valid nucleoli channel" % (nuclei[index].split("TIF_GZ/"))[1].split("blue")[0])
         continue  
     
     try:
@@ -61,10 +67,11 @@ for index,imgpath in enumerate(nuclei):
             if len(mi.shape) > 2:
                 mi=mi[:,:,0]
     except:
+        print("%s does not have valid microtubule channel" % (nuclei[index].split("TIF_GZ/"))[1].split("blue")[0])
         continue      
 
     # obtain nuclei seed for watershed segmentation
-    seed, num = watershed_lab(nu)
+    seed, num = watershed_lab(nu, rm_border=False)
     
     # segment microtubule image
     marker = np.full_like(seed, 0)
@@ -93,24 +100,22 @@ for index,imgpath in enumerate(nuclei):
         mask[mask != region.label] = 0
         mask[mask == region.label] = 1
 
-        cell_nuclei = nu[minr:maxr,minc:maxc]*mask
-        cell_nucleoli = org[minr:maxr,minc:maxc]*mask
-        cell_microtubule = mi[minr:maxr,minc:maxc]*mask
+        cell_nuclei = pixel_norm(nu[minr:maxr,minc:maxc]*mask)
+        cell_nucleoli = pixel_norm(org[minr:maxr,minc:maxc]*mask)
+        cell_microtubule = pixel_norm(mi[minr:maxr,minc:maxc]*mask)
 
         # stack channels
-        cell = np.dstack((cell_microtubule,cell_nucleoli,cell_nuclei))
-        if cell.dtype != 'uint8' :
-            cell = (cell/255).astype(np.uint8) #the input file was uint16
+        cell = np.dstack((cell_microtubule,cell_nucleoli,cell_nuclei))         
+        cell = (cell*255).astype(np.uint8) #the input file was uint16         
 
         # align cell to the 1st major axis  
         theta=region.orientation*180/np.pi #radiant to degree conversion
         cell = ndi.rotate(cell, 90-theta)
-         
+
         # resize images
         fig = resize_pad(cell) # default size is 256x256
         # center to the center of mass of the nucleus
         fig = shift_center_mass(fig)
-        
         fig = Image.fromarray(fig)
         name = "%s_cell%s.%s" % ((nuclei[index].split("TIF_GZ/"))[1].split("blue")[0],str(i), "png")
         name = name.replace("/", "_")
@@ -118,4 +123,4 @@ for index,imgpath in enumerate(nuclei):
         savepath= os.path.join(imageoutput, name)
         #plt.savefig(savepath)
         fig.save(savepath)
-        
+
